9726dbf35582837c9fcf1d3d22837d1701d3ae76
vishkj <vjanagoud@focusvision.com>
2018-09-07 15:27:02 +0530

    Project Diya: Initial commit with project start.

---
+++ src/main/java/com/projectdiya/ProjectDiya/model/User.java
@@ -0,0 +1,20 @@
+package com.projectdiya.ProjectDiya.model;
+
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", getUserName()=" + getUserName() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
+}
